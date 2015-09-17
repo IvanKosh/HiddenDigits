1554f90bf968e89f51f00f1341be590f71a25853
Batiatari Kaze <batiatari.dono@gmail.com>
2015-09-17 17:05:08 +0300

    finish

--- main.cpp
+++ main.cpp
@@ -5,15 +5,29 @@
  * Created on 17 Сентябрь 2015 г., 17:01
  */
 
-#include <cstdlib>
-
+#include <iostream>
+#include <fstream>
 using namespace std;
-
-/*
- * 
- */
-int main(int argc, char** argv) {
-
-	return 0;
+int main(int argc, char *argv[]) {
+    ifstream stream(argv[1]);
+    string line;
+    int i, l;
+    bool t;
+    while (getline(stream, line)) {
+        t = true;
+        l = line.size();
+        for (i = 0; i < l; i++) {
+            if('/' < line[i] && line[i] < ':') {
+                t = false;
+                cout << line[i];
+            }
+            if('`' < line[i] && line[i] < 'k') {
+                t = false;
+                printf("%c", line[i]-49);
+            }
+        }
+        if (t) cout << "NONE";
+        cout << '\n';
+    }
+    return 0;
 }
-
