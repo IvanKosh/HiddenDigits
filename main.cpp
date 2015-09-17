0b276d173f5f371f03f25facb95bfb36adf4dc2e
Batiatari Kaze <batiatari.dono@gmail.com>
2015-09-17 17:02:50 +0300

    start

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: bat
+ *
+ * Created on 17 Сентябрь 2015 г., 17:01
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+	return 0;
+}
+
